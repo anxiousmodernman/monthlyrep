9464efa4dcef710bb0467ea7d673190dc12bf199
ahelium <anna.glander@gmail.com>
2014-11-12 17:21:12 -0500

    '-m Unsubs to unsubs'

--- Unsubs/unsubs.py
+++ Unsubs/unsubs.py
@@ -7,7 +7,7 @@ def combine(df):
     df['ActiveUnsubs'] = df['Feedback Loop - ACTIVE'] + df['Feedback - ACTIVE'] + df['Web Site - ACTIVE'] + df['Android - ACTIVE']
     df['Other'] = df['Other'] + df['Postpone - NON-UNSUB'] + df['Website Update Email - NON-UNSUB']
     df['DeliveredMail'] = df['Messages Sent'] - df['Hard Bounce - INACTIVE']
-    Metrics = df[['Beginning Subs', 'Ending Subs', 'DeliveredMail', 'Total Unsubs', 'Inactive Removal - INACTIVE', 'Hard Bounce - INACTIVE', 'ActiveUnsubs', 'Other']].sum()
+    Metrics = df[['Beginning Subs', 'Ending Subs', 'DeliveredMail', 'Total unsubs', 'Inactive Removal - INACTIVE', 'Hard Bounce - INACTIVE', 'ActiveUnsubs', 'Other']].sum()
     data = pd.DataFrame(Metrics)
     return data
 
